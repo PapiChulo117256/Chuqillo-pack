d013ca5f931d0ae3f17ccba719b2eaf3e720903f
PapiChulo117256 <yahirdhidalgo11@gmail.com>
2025-06-17 13:02:27 -0500

    Create ChuqilloMod.java
    🔥 Inicio del Chuqillo Pack - clase principal del mod

---
+++ src/main/java/com/chuquillopack/ChuqilloMod.java
@@ -0,0 +1,13 @@
+package com.chuquillopack;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ChuqilloMod implements ModInitializer {
+    public static final String MOD_ID = "chuquillopack";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Chuqillo Pack Activado!");
+        // Aquí van los registros de cuchillos, encantamientos y aldeanos
+    }
+}
