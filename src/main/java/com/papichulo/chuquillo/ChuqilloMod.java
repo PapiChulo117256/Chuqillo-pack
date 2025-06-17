687f3190ba2a33fdec03af13a5ab111356e6124e
PapiChulo117256 <yahirdhidalgo11@gmail.com>
2025-06-17 13:19:06 -0500

    Create ChuqilloMod.java
    Archivo principal ChuqilloMod ubicado correctamente

---
+++ src/main/java/com/papichulo/chuquillo/ChuqilloMod.java
@@ -0,0 +1,10 @@
+package com.papichulo.chuqillo;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ChuqilloMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("🔥 El Chuqillo Pack se ha cargado correctamente!");
+    }
+}
